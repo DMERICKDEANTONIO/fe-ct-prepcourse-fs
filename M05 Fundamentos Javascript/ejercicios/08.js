4e85c45425377f4522eb349fd39feadc8d8f0a14
erickdeantonio <erickdeantoniom@gmail.com>
2024-01-23 17:51:05 -0500

    ejercicios resueltos del 1-18

--- M05 Fundamentos Javascript/ejercicios/08.js
+++ M05 Fundamentos Javascript/ejercicios/08.js
@@ -2,7 +2,13 @@ function esNumeroEntero(numero) {
   // La función recibe un argumento "numero".
   // Verifica si este es un número entero o no.
   // Returna true si lo es, de lo contrario, retorna false.
-  // Tu código:
-}
+  // Tu código: 
+  if (numero == 'numeroEntero'){
+    return true
+  }
+  return false
+    
+  }
 
-module.exports = esNumeroEntero;
+
+module.exports = esNumeroEntero ;
--- M05 Fundamentos Javascript/ejercicios/14.js
+++ M05 Fundamentos Javascript/ejercicios/14.js
@@ -3,6 +3,10 @@ function tienenMismaLongitud(str1, str2) {
   // Retorna true si los dos strings tienen la misma longitud.
   // De lo contrario, retorna false.
   // Tu código:
+  if("str1"==="str2"){
+    return( true );
+  }
+  return( false );
 }
 
 module.exports = tienenMismaLongitud;
