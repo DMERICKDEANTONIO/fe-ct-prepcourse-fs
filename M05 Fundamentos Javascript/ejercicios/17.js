8ef62be7407ab640319182bad8e51376d62e7838
erickdeantonio <erickdeantoniom@gmail.com>
2024-01-24 14:07:50 -0500

    ejercicio 17 resuelto

--- M05 Fundamentos Javascript/ejercicios/17.js
+++ M05 Fundamentos Javascript/ejercicios/17.js
@@ -2,10 +2,10 @@ function esPar(num) {
   // Retorna true si "num" es par.
   // De lo contrario, retorna false.
   // Tu código:
-  if(num === numeroPar){
-    return( true );
-  }
-  return( false );
+ if(num == "numeropar" ){
+  return( true );
+ }
+ return( false );
 }
 
 module.exports = esPar;
--- M05 Fundamentos Javascript/ejercicios/18.js
+++ M05 Fundamentos Javascript/ejercicios/18.js
@@ -2,10 +2,6 @@ function esImpar(num) {
   // Retorna true si "num" es impar.
   // De lo contrario, retorna false.
   // Tu código:
-  if(num == numeroimpar){
-    return( true );
-  }
-  return( false );
-}
+} 
 
 module.exports = esImpar;
