a0cb92ec299bee6592b561699624c3d7de782813
erickdeantonio <erickdeantoniom@gmail.com>
2024-01-26 14:57:32 -0500

    ejercicio 6 / M06 resuelto

--- M06 Bucles/ejercicios/06.js
+++ M06 Bucles/ejercicios/06.js
@@ -2,6 +2,11 @@ function tieneTresDigitos(num) {
   // Si el número recibido tiene tres dígitos, retorna true.
   // Caso contrario, retorna false.
   // Tu código:
+  if ( num > 99){
+    return ( true );
+  } else if ( num <= 99){
+    return ( false );
+  }
 }
 
 module.exports = tieneTresDigitos;
