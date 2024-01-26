8933e740d529ab16baaf3e7a4e72447873e4f0ce
erickdeantonio <erickdeantoniom@gmail.com>
2024-01-26 14:29:41 -0500

    ajercicio 3 / M06 resuelto

--- M06 Bucles/ejercicios/03.js
+++ M06 Bucles/ejercicios/03.js
@@ -3,6 +3,14 @@ function obtenerMayor(x, y) {
   // Retorna el número más grande.
   // Si son iguales, retornar cualquiera de los dos.
   // Tu código:
+  if (x < y){
+    return ( y );
+  } else if(y < x ){
+    return ( x );
+  }else {
+    return Math.random(x || y);
+  }
+
 }
 
 module.exports = obtenerMayor;
