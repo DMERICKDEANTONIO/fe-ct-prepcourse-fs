978c9d65b5441798d6200c2b5deb5bd15dc1ac30
erickdeantonio <erickdeantoniom@gmail.com>
2024-01-30 14:46:52 -0500

    ejercicio 26 M06 resuelto

--- M06 Bucles/ejercicios/26.js
+++ M06 Bucles/ejercicios/26.js
@@ -5,19 +5,18 @@ function esAnagrama(str1, str2) {
   // IMPORTANTE: Un anagrama es una palabra que se forma
   // con las mismas letras que otra, pero en orden diferente.
   // Tu código:
-   // Convertir las cadenas a arreglos de caracteres
+
    var arr1 = str1.split("");
    var arr2 = str2.split("");
  
-   // Ordenar los arreglos alfabéticamente
+  
    arr1.sort();
    arr2.sort();
  
-   // Convertir los arreglos de nuevo a cadenas
+
    var nuevaStr1 = arr1.join("");
    var nuevaStr2 = arr2.join("");
  
-   // Comparar las cadenas resultantes
    if (nuevaStr1 === nuevaStr2) {
      return true;
    } else {
