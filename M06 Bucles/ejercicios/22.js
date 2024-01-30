3077beaca9ca80d5dbd70878694d7eb260a7425e
erickdeantonio <erickdeantoniom@gmail.com>
2024-01-30 11:57:59 -0500

    ejercicios del 21 al 26 M06 resueltos

--- M06 Bucles/ejercicios/22.js
+++ M06 Bucles/ejercicios/22.js
@@ -3,6 +3,18 @@ function diasEnMes(mes) {
   // Determine cúantos días tiene el mes correspondiente a ese número.
   // PISTA: Usa un bucle do-while para validar que el mes ingresado sea válido.
   // Tu código:
+  let days = 0;
+  do {
+    if (mes === 2) {
+      days = 28;
+    } else if (mes === 4 || mes === 6 || mes === 9 || mes === 11) {
+      days = 30;
+    } else {
+      days = 31;
+    }
+  } while (false);
+  return days;
+
 }
 
 module.exports = diasEnMes;
--- M06 Bucles/ejercicios/25.js
+++ M06 Bucles/ejercicios/25.js
@@ -5,6 +5,8 @@ function esPalindromo(string) {
   // IMPORTANTE: Un palíndromo es una palabra o frase
   // que se lee igual hacia adelante que hacia atrás.
   // Tu código:
+  let reversedString = string.split("").reverse().join("");
+  return string === reversedString;
 }
 
 module.exports = esPalindromo;
