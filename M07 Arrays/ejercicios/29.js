413ca635da9d61e2d92e36d663dad1a3bac251bc
erickdeantonio <erickdeantoniom@gmail.com>
2024-02-01 15:02:04 -0500

    ejercicios del 21 al 30/ M07 resuelto

--- M07 Arrays/ejercicios/29.js
+++ M07 Arrays/ejercicios/29.js
@@ -4,6 +4,28 @@ function encontrarNumeroFaltante(numeros) {
   // y retórnalo.
   // Devuelve null si el aray es vacío o si no hay números faltantes.
   // Tu código:
+  let sumaTotal = 0;
+  let sumaParcial = 0;
+  let numeroFaltante = null;
+
+  for (let i = 0; i < numeros.length; i++) {
+    sumaTotal += numeros[i];
+  }
+
+  for (let i = 1; i <= numeros.length + 1; i++) {
+    sumaParcial += i;
+  }
+
+  numeroFaltante = sumaParcial - sumaTotal;
+
+  if (numeroFaltante === 0) {
+    return null;
+  } else {
+    return numeroFaltante;
+  }
+  
+
+  
 }
 
 module.exports = encontrarNumeroFaltante;
--- M07 Arrays/ejercicios/30.js
+++ M07 Arrays/ejercicios/30.js
@@ -2,6 +2,17 @@ function encontrarElementoRepetido(numeros) {
   // La función recibe un argumento "numeros" que es un array de números.
   // Retorna el primer elemento repetido que se encuentre en el array.
   // Tu código:
+
+ let numerosRepetidos = [];
+
+ for (let i = 0; i < numeros.length; i++) {
+   if (numeros.indexOf(numeros[i]) !== i && !numerosRepetidos.includes(numeros[i])) {
+     numerosRepetidos.push(numeros[i]);{
+      break;
+     }
+   }
+ }
+   return numerosRepetidos[0];
 }
 
 module.exports = encontrarElementoRepetido;
