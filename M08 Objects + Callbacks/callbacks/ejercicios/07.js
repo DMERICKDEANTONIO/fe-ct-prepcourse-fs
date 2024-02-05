f55f50428a0995633ee46c11ffe7087960c5898d
erickdeantonio <erickdeantoniom@gmail.com>
2024-02-05 18:17:20 -0500

    ejercicios 5,6,7 del M08 resueltos

--- M08 Objects + Callbacks/callbacks/ejercicios/07.js
+++ M08 Objects + Callbacks/callbacks/ejercicios/07.js
@@ -2,6 +2,12 @@ function filter(arrayOfStrings) {
   // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
   // Luego retorna un nuevo arreglo con estos elementos.
   // Tu código:
+ let nuevoArray = []
+ nuevoArray = arrayOfStrings.filter(function(ele){
+  return ele[0] === "a" || ele [0] === "A"
+ })
+ return nuevoArray;
 }
+ 
 
 module.exports = filter;
