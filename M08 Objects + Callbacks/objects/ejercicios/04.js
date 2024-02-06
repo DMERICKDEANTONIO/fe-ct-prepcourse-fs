746a96744d8a1bcb6aa51aa4ad4bdd1da396a98c
erickdeantonio <erickdeantoniom@gmail.com>
2024-02-05 20:02:19 -0500

    ejercicios del 1 al 5/ M08 resueltos

--- M08 Objects + Callbacks/objects/ejercicios/04.js
+++ M08 Objects + Callbacks/objects/ejercicios/04.js
@@ -3,6 +3,8 @@ function verificarPropiedad(objeto, propiedad) {
   // Retorna true si la tiene, sino retorna false.
   // PISTA: Puedes usar el método hasOwnProperty().
   // Tu código:
+   var tienePropiedad = objeto.hasOwnProperty(propiedad)
+   return tienePropiedad;
 }
 
 module.exports = verificarPropiedad;
--- M08 Objects + Callbacks/objects/ejercicios/05.js
+++ M08 Objects + Callbacks/objects/ejercicios/05.js
@@ -2,6 +2,9 @@ const listarPropiedades = (objeto) => {
   // Lista y retorna todas las propiedades que posee el objeto recibido por la función.
   // PISTA: Puedes usar el método Object.keys().
   // Tu código:
+  let objetos = Object.keys(objeto)
+  return objetos
+
 };
 
 module.exports = listarPropiedades;
