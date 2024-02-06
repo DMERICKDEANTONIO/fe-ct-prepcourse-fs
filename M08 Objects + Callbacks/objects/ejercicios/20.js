2da7e31dc9f9bc33a4cee3d130de59e6a44bf8e9
erickdeantonio <erickdeantoniom@gmail.com>
2024-02-06 14:26:11 -0500

    ejercicio 20 del M08 (objects) resuelto

--- M08 Objects + Callbacks/objects/ejercicios/20.js
+++ M08 Objects + Callbacks/objects/ejercicios/20.js
@@ -4,6 +4,12 @@ function sumarLikesDeUsuario(objetoUsuario) {
   // Cada post posee una propiedad llamada "likes". Esta propiedad es un número.
   // Debes sumar los likes de todos los post y retornar el resultado.
   // Tu código:
+  var resultados = 0
+  for (var i = 0 ; i < objetoUsuario.posts.length; i++){
+    resultados = resultados + objetoUsuario.posts[i].likes;
+  }
+return resultados;
+
 }
 
 module.exports = sumarLikesDeUsuario;
